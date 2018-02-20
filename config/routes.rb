553075b1c5a609c76612e47beca27ecc80d1a634
sebastienrombaut <sebastien.rombaut@hotmail.fr>
2018-02-20 13:13:57 +0100

    flynn over

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   root 'home#flynn'
 
+  get 'home/flynn'
+
   get 'home/sparks'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
